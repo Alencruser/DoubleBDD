da4980478cae4bddc56ed92662fdf685703573b4
Simplejul <simplejul@protonmail.com>
2018-06-18 15:57:12 +0200

    connexion serveur mysql

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-var express = require('express');
-var app = express();
-var mysql = require('mysql');
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
 
-var server = app.listen(3000, (req,res) => 
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'toor',
+    database: 'DoubleBDD'
+});
+
+connection.connect(function(err){
+    if (err){
+        return console.error('error: ' + err.message);
+    }
+    console.log('Connected to the MySQL server.');    
+});
+
+const server = app.listen(3000, (req,res) => 
 console.log('Server Ready')
 );
 
