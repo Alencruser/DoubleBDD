6c799142ac5fee517e8bfd7bf2872b7f620a6f78
Valentin Janvier <alencruserxx@hotmail.fr>
2018-06-20 16:03:16 +0200

    switch de fichier pour les tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,18 @@
-var expect = require('chai').expect;
+var expect= require('chai').expect;
+var mysql=require('mysql');
+var addpost=require('../addpost');
 
-console.log("test");
+describe('',function(){
+	it('should receive an input',function(){
+
+		let connection = mysql.createConnection({
+    		host: 'localhost',
+    		user: 'root',
+    		password: 'toor',
+    		database: 'DoubleBDD'
+		});
+		var success = "authenticated";
+		var testing = addpost(connection.state);
+		expect(success).to.be.equal(testing);
+	})
+})
